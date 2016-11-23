6c4c2bd8cd0fbe10b7df354f46c60b71584d3bfc
Shusen Wang <wssatzju@gmail.com>
2016-11-23 13:15:06 -0800

    added python code for processing LibSVM data

---
+++ resource/processLibSVMData.py
@@ -0,0 +1,30 @@
+import numpy
+
+
+def parseRawDataUtil(idxValStr):
+    idxValPair = idxValStr[2:-1].split(':')
+    return [int(idxValPair[0]), float(idxValPair[1])]
+
+def parseRawData(rawData, d):
+    labelVec = numpy.zeros(d + 1)
+    labelVec[0] = float(rawData[0][2:-1])
+    vecStr = rawData[1:]
+    vecIdxVal = list(map(parseRawDataUtil, vecStr))
+    for idxValPair in vecIdxVal:
+        labelVec[idxValPair[0]] = idxValPair[1]
+    return labelVec
+
+
+if __name__ == '__main__':
+    inputFileName = 'YearPredictionMSD.t'
+    outputFileName = 'YearPredictionMSD.t.npy'
+    d = 90 # number of features
+    
+    listArrayStr = numpy.loadtxt(inputFileName, dtype='str')
+    n = len(listArrayStr)
+    mat = numpy.zeros((n, d+1))
+    for i in range(n):
+        rawData = listArrayStr[i]
+        mat[i, :] = parseRawData(rawData, d)
+    
+    numpy.save(outputFileName, mat)
