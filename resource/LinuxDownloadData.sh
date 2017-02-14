039e9f1af9b66b1edcccc07ac4e0c4dba6cd0d66
Shusen Wang <wssatzju@gmail.com>
2017-02-13 20:14:36 -0800

    changed data downloading scripts

--- resource/LinuxDownloadData.sh
+++ resource/LinuxDownloadData.sh
@@ -1,6 +1,6 @@
-cd ./resource
+#!/usr/bin/env bash
+
 wget http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/regression/YearPredictionMSD.bz2
 wget http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/regression/YearPredictionMSD.t.bz2
 bzip2 -d YearPredictionMSD.bz2
 bzip2 -d YearPredictionMSD.t.bz2
-cd ..
---
+++ resource/MacDownloadData.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+curl "http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/regression/YearPredictionMSD.bz2" -o "YearPredictionMSD.bz2"
+curl "http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/regression/YearPredictionMSD.t.bz2" -o "YearPredictionMSD.t.bz2"
+bzip2 -d YearPredictionMSD.bz2
+bzip2 -d YearPredictionMSD.t.bz2
